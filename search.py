fc07771057cf326299bd82d6b091b3e205970aec
cl <creightonlewis01@gmail.com>
2025-10-23 15:16:38 -0500

    Update search.py

--- search.py
+++ search.py
@@ -1,3 +1,8 @@
+import os 
+import argparse 
+import xmltodict
+
 # Something that parses and finds out the host from the list 
-Nmap scan for 
+Nmap scan for {host}
+https://vulners.com/CVE-2000-1000
 # Something that searches for the CVE-0000-00000
